674c5867682905c932fbfd91be159bef94048864
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-06-20 07:15:29 +0530

    this is new commit

--- hellllllllllp/hellllllllllp/urls.py
+++ hellllllllllp/hellllllllllp/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from helpApp.views import StudentViewSet
+# from rest_framework.routers import DefaultRouter
+# from helpApp.views import StudentViewSet
 
 # router=DefaultRouter()
 # router.register('students',StudentViewSet)
--- hellllllllllp/helpApp/urls.py
+++ hellllllllllp/helpApp/urls.py
@@ -6,6 +6,6 @@ from .views import student_details, get_students
 urlpatterns = [
     # path('students/', StudentsListCreate.as_view(), name='get_students'),
     # path('student/<int:pk>/', StudentRetriveUpdateDelete.as_view(), name='student_details'),
-        path('students/', student_details, name='get_students'),
-    path('student/<int:pk>/', get_students, name='student_details'),
+        path('students/', get_students, name='get_students'),
+    path('student/<int:pk>/',student_details , name='student_details'),
 ]
